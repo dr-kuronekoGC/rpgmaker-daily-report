51323f63d24b3c59af7d1362b787d552298ad3e1
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-07-08 18:16:45 +0900

    Update common.py

--- common.py
+++ common.py
@@ -1,29 +1,49 @@
 import json
 
 from pathlib import Path
-from datetime import datetime, timedelta
+from datetime import datetime, UTC, timedelta
+
 
 # ----------------------------
 # 日本時間
 # ----------------------------
 
 def now_jst():
-    return datetime.utcnow() + timedelta(hours=9)
+    return datetime.now(UTC) + timedelta(hours=9)
+
 
 def get_period():
+
     hour = now_jst().hour
+
     if 5 <= hour < 10:
         return "朝"
+
     elif 10 <= hour < 15:
         return "昼"
+
     elif 15 <= hour < 18:
         return "夕"
+
     else:
         return "夜"
+
 
 # ----------------------------
 # seen ファイル
 # ----------------------------
+
+def load_seen_file(filename):
+
+    path = Path(filename)
+
+    if not path.exists():
+        return []
+
+    with open(path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
 def save_seen_file(filename, data):
 
     with open(filename, "w", encoding="utf-8") as f:
