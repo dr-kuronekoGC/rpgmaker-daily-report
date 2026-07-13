da74294b806255efd41ab4753e7cba6a235b6a66
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-07-13 16:34:04 +0900

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,1 +1,13 @@
+from . import reddit
+from . import official
 
+SOURCES = (
+    {
+        "module": reddit,
+        "seen": "seen.json",
+    },
+    {
+        "module": official,
+        "seen": "seen_official.json",
+    },
+)
