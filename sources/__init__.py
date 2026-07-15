616441f00f2dc98dba7450a0dbf6cbe76b2bc124
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-07-15 11:59:02 +0900

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,28 +1,10 @@
-from config import (
-    SEEN_FILE,
-    SEEN_OFFICIAL_FILE,
-    FORUM_SEEN_FILE,
-)
-
 from . import reddit
 from . import official
 from . import forum
 
+
 SOURCES = (
-
-    {
-        "module": reddit,
-        "seen": SEEN_FILE,
-    },
-
-    {
-        "module": official,
-        "seen": SEEN_OFFICIAL_FILE,
-    },
-
-    {
-        "module": forum,
-        "seen": FORUM_SEEN_FILE,
-    },
-
+    reddit,
+    official,
+    forum,
 )
