28ffa8d3285cf63cc060171b5cc9276b0472ed0c
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-07-15 11:52:06 +0900

    Update reddit.py

--- sources/reddit.py
+++ sources/reddit.py
@@ -1,10 +1,11 @@
 from config import (
-    RSS_URL,
     REDDIT_SEEN_FILE,
 )
 
 from categories import classify_reddit
+
 from sources.rss import collect_rss
+
 
 SEEN_FILE = REDDIT_SEEN_FILE
 
@@ -12,7 +13,6 @@ SEEN_FILE = REDDIT_SEEN_FILE
 def get_items(seen):
 
     return collect_rss(
-        url=RSS_URL,
         seen=seen,
         classify=classify_reddit,
         source_name="Reddit",
