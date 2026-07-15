84f07520d22080db49a862bfcec5732212fe457a
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-07-15 13:02:38 +0900

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,10 +1,17 @@
 from . import community_reddit
 from . import community_forum
+
 from . import official_site
+from . import official_steam
 
 
 SOURCES = (
+
     community_reddit,
+
     official_site,
+    official_steam,
+
     community_forum,
+
 )
