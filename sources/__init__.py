070ba8397684843bd06ff2ecccf4ad3f68a16ed8
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-07-27 16:15:45 +0900

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -4,16 +4,14 @@ from . import community_forum
 from . import official_site
 from . import official_steam
 from . import official_itchio
-
+from . import official_opengameart
 
 SOURCES = (
-
-    community_reddit,
-
     official_site,
     official_steam,
     official_itchio,
+    official_opengameart,
 
     community_forum,
-
+    community_reddit,
 )
