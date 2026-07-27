24268c0ee58e1be67a7dcafa6bfb0904d2dff8fc
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-07-27 16:22:12 +0900

    Update official_itchio.py

--- sources/official_itchio.py
+++ sources/official_itchio.py
@@ -1,11 +1,11 @@
 from config import (
-    ITCHIO_URL,
+    ITCHIO_ASSET_RSS,
     ITCHIO_SEEN_FILE,
 )
 
-from categories import classify_itchio
-
 from sources.rss import collect_rss
+
+from categories import classify_itch
 
 
 SEEN_FILE = ITCHIO_SEEN_FILE
@@ -14,8 +14,8 @@ SEEN_FILE = ITCHIO_SEEN_FILE
 def get_items(seen):
 
     return collect_rss(
-        url=ITCHIO_URL,
+        url=ITCHIO_ASSET_RSS,
         seen=seen,
-        classify=classify_itchio,
+        classify=classify_itch,
         source_name="itch.io",
     )
