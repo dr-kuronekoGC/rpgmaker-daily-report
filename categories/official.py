b61a63d6e9bafcddf9adba0f40392a9db3457c99
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-07-31 21:58:45 +0900

    Update official.py

--- categories/official.py
+++ categories/official.py
@@ -1,1 +1,12 @@
-classify_official()
+# ==========================================
+# Official
+# ==========================================
+
+def classify_official(title):
+
+    title = title.lower()
+
+    if "unite" in title:
+        return "UNITE"
+
+    return "本体ニュース"
