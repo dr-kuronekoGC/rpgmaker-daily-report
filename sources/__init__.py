66fdcdc4745e50bfc1bf7b2ce10ef57e9182e41c
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-01 13:31:02 +0900

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -3,6 +3,7 @@ from . import community_forum
 
 from . import official_site
 from . import official_steam
+
 from . import official_opengameart
 from . import official_kenney
 from . import official_craftpix
@@ -10,12 +11,14 @@ from . import official_gamedevmarket
 
 from . import asset_itchio
 
+
 SOURCES = (
 
     community_reddit,
 
     official_site,
     official_steam,
+
     official_opengameart,
     official_kenney,
     official_craftpix,
