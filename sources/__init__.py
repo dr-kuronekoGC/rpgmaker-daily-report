3d1e5168b8a8c2af5344a07bfa7baedc56542857
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-01 18:53:41 +0900

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -8,6 +8,7 @@ from . import official_opengameart
 from . import official_kenney
 from . import official_craftpix
 from . import official_gamedevmarket
+from . import official_visustella
 
 from . import asset_itchio
 
@@ -23,6 +24,7 @@ SOURCES = (
     official_kenney,
     official_craftpix,
     official_gamedevmarket,
+    official_visustella,
 
     asset_itchio,
 
