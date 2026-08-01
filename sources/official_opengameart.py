01dc50893bc5660ca0ebde352301af7c69cfd38e
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-01 19:08:36 +0900

    Update official_opengameart.py

--- sources/official_opengameart.py
+++ sources/official_opengameart.py
@@ -1,14 +1,11 @@
-from urllib.parse import urljoin
-
-from bs4 import BeautifulSoup
-
 from config import (
-    OPENGAMEART_URL,
+    OPENGAMEART_RSS,
     OPENGAMEART_SEEN_FILE,
 )
 
-from categories import classify_opengameart
-from sources.base import get_html
+from sources.rss import collect_rss
+
+from categories.asset import classify_asset
 
 
 SEEN_FILE = OPENGAMEART_SEEN_FILE
@@ -16,78 +13,9 @@ SEEN_FILE = OPENGAMEART_SEEN_FILE
 
 def get_items(seen):
 
-    html = get_html(OPENGAMEART_URL)
-
-    soup = BeautifulSoup(
-        html,
-        "html.parser",
+    return collect_rss(
+        url=OPENGAMEART_RSS,
+        seen=seen,
+        classify=classify_asset,
+        source_name="OpenGameArt",
     )
-
-    adopted_items = []
-
-    new_seen = seen.copy()
-
-    seen_urls = set()
-
-    #
-    # OpenGameArtは作品一覧のリンクを取得
-    #
-    for link in soup.select("a[href]"):
-
-        href = link.get("href")
-
-        if not href:
-            continue
-
-        #
-        # artworkのみ取得
-        #
-        if "/content/" not in href:
-            continue
-
-        href = urljoin(
-            OPENGAMEART_URL,
-            href,
-        )
-
-        if href in seen_urls:
-            continue
-
-        seen_urls.add(href)
-
-        if href in seen:
-            continue
-
-        title = link.get_text(
-            " ",
-            strip=True,
-        )
-
-        if len(title) < 4:
-            continue
-
-        category = classify_opengameart(title)
-
-        if category is None:
-            continue
-
-        new_seen.append(href)
-
-        adopted_items.append(
-            {
-                "title": title,
-                "url": href,
-                "category": category,
-                "source": "OpenGameArt",
-            }
-        )
-
-        print(
-            f"[OpenGameArt][{category}] {title}"
-        )
-
-    print(
-        f"[OpenGameArt] New: {len(adopted_items)}"
-    )
-
-    return adopted_items, new_seen
