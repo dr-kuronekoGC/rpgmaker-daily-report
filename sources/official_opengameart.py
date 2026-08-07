e6fe3ae23015c7d6a230672b838ff5d9d86c54fe
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-07 16:17:05 +0900

    Update official_opengameart.py

--- sources/official_opengameart.py
+++ sources/official_opengameart.py
@@ -1,11 +1,10 @@
 from config import (
-    OPENGAMEART_RSS,
+    OPENGAMEART_URL,
     OPENGAMEART_SEEN_FILE,
 )
 
-from sources.rss import collect_rss
-
 from categories.asset import classify_asset
+from sources.html import collect_html
 
 
 SEEN_FILE = OPENGAMEART_SEEN_FILE
@@ -13,9 +12,10 @@ SEEN_FILE = OPENGAMEART_SEEN_FILE
 
 def get_items(seen):
 
-    return collect_rss(
-        url=OPENGAMEART_RSS,
+    return collect_html(
+        url=OPENGAMEART_URL,
         seen=seen,
         classify=classify_asset,
+        selector="a[href]",
         source_name="OpenGameArt",
     )
