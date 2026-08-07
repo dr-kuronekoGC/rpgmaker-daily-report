6a194f0d1b6e7e5942f4e916ccfb300f2550179d
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-07 15:46:56 +0900

    Update official_gamedevmarket.py

--- sources/official_gamedevmarket.py
+++ sources/official_gamedevmarket.py
@@ -1,1 +1,21 @@
+from config import (
+    GAMEDEVMARKET_URL,
+    GAMEDEVMARKET_SEEN_FILE,
+)
+
 from categories.asset import classify_asset
+from sources.html import collect_html
+
+
+SEEN_FILE = GAMEDEVMARKET_SEEN_FILE
+
+
+def get_items(seen):
+
+    return collect_html(
+        url=GAMEDEVMARKET_URL,
+        seen=seen,
+        classify=classify_asset,
+        selector="a[href]",
+        source_name="GameDevMarket",
+    )
