a465a007776375422ae658b095e1cda3f563b377
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-10 17:37:25 +0900

    keywords.py を作成

---
+++ categories/keywords.py
@@ -0,0 +1,61 @@
+# ==========================================
+# Common Classification Keywords
+# ==========================================
+
+SOUND_KEYWORDS = (
+    "music",
+    "bgm",
+    "sound",
+    "sfx",
+    "audio",
+)
+
+GRAPHIC_KEYWORDS = (
+    "asset",
+    "assets",
+    "resource",
+    "resources",
+    "tileset",
+    "sprite",
+    "portrait",
+    "faceset",
+    "character",
+    "pixel",
+    "icon",
+    "background",
+    "ui",
+    "graphic",
+    "art",
+)
+
+PLUGIN_KEYWORDS = (
+    "plugin",
+    "plugins",
+    "script",
+    "tool",
+    "extension",
+)
+
+GAME_KEYWORDS = (
+    "game",
+    "release",
+    "released",
+    "demo",
+    "chapter",
+    "episode",
+    "beta",
+    "alpha",
+    "trailer",
+)
+
+QUESTION_KEYWORDS = (
+    "help",
+    "question",
+    "how",
+    "why",
+    "looking for",
+    "need",
+    "error",
+    "issue",
+    "problem",
+)
