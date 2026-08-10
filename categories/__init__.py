422a1f4b3104a2b0358ec0bc40760764f4b9cddc
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-10 17:18:33 +0900

    __init__.py を更新

--- categories/__init__.py
+++ categories/__init__.py
@@ -1,31 +1,8 @@
-# ==========================================
-# Categories
-# ==========================================
+from .display import DISPLAY_CATEGORY
 
-from .display import (
-    DISPLAY_CATEGORY,
-)
-
-from .official import (
-    classify_official,
-)
-
-from .reddit import (
-    classify_reddit,
-)
-
-from .forum import (
-    classify_forum,
-)
-
-from .steam import (
-    classify_steam,
-)
-
-from .itch import (
-    classify_itch,
-)
-
-from .assets import (
-    classify_asset,
-)
+from .official import classify_official
+from .reddit import classify_reddit
+from .forum import classify_forum
+from .steam import classify_steam
+from .itch import classify_itch
+from .assets import classify_asset
