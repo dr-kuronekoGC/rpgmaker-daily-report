16ed2ad1267e16d742d34ba17efbe7df9f4b10af
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-10 17:41:34 +0900

    itch.py を更新

--- categories/itch.py
+++ categories/itch.py
@@ -2,51 +2,18 @@
 # itch.io Classification
 # ==========================================
 
-PLUGIN_KEYWORDS = (
-    "plugin",
-    "plugins",
-    "script",
-    "tool",
-    "extension",
-)
-
-SOUND_KEYWORDS = (
-    "music",
-    "bgm",
-    "sound",
-    "sfx",
-    "audio",
-)
-
-GRAPHIC_KEYWORDS = (
-    "asset",
-    "assets",
-    "tileset",
-    "sprite",
-    "portrait",
-    "faceset",
-    "character",
-    "pixel",
-    "icon",
-    "background",
-    "ui",
-    "graphic",
-    "art",
-)
-
-GAME_KEYWORDS = (
-    "game",
-    "demo",
-    "release",
-    "chapter",
-    "episode",
-    "beta",
-    "alpha",
-    "trailer",
+from .keywords import (
+    SOUND_KEYWORDS,
+    GRAPHIC_KEYWORDS,
+    PLUGIN_KEYWORDS,
+    GAME_KEYWORDS,
 )
 
 
-def _contains(title, keywords):
+def _contains(
+    title,
+    keywords,
+):
     return any(
         keyword in title
         for keyword in keywords
