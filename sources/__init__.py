e470622dd5db486a5f02415b6cf16076f6ba7f08
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-10 16:48:29 +0900

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,41 +1,22 @@
-# ==========================================
-# Sources
-# ==========================================
-
-from . import community_reddit
-from . import official_site
+from . import reddit
 from . import official_steam
-
 from . import official_opengameart
 from . import official_kenney
 from . import official_craftpix
 from . import official_gamedevmarket
 from . import official_visustella
-
 from . import asset_itchio
+from . import forum
 
-from . import community_forum
-
-
-# ==========================================
-# Source List
-# ==========================================
 
 SOURCES = [
-
-    community_reddit,
-
-    official_site,
+    reddit,
     official_steam,
-
     official_opengameart,
     official_kenney,
     official_craftpix,
     official_gamedevmarket,
     official_visustella,
-
     asset_itchio,
-
-    community_forum,
-
+    forum,
 ]
