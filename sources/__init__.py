7b6688d521ae1661c820834cc93f0b1175c9e3ac
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-11 22:33:27 +0900

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -7,5 +7,4 @@ from . import official_kenney
 from . import official_craftpix
 from . import official_gamedevmarket
 from . import official_visustella
-from . import official_itchio
 from . import asset_itchio
