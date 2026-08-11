45fa37d9ca5c0d927bd51cef8af909390102c843
dr-kuronekoGC <t-ond@hotmail.co.jp>
2026-08-11 21:01:09 +0900

    Update itch.io RSS feed URLs for RPG Maker

--- config.py
+++ config.py
@@ -61,11 +61,11 @@ SLACK_WEBHOOK_ENV = "SLACK_WEBHOOK_URL"
 # ==========================================
 
 ITCHIO_GAME_RSS = (
-    "https://itch.io/games/tag-rpg-maker"
+    "https://itch.io/games/tag-rpgmaker"
 )
 
 ITCHIO_ASSET_RSS = (
-    "https://itch.io/game-assets/tag-rpg-maker"
+    "https://itch.io/game-assets/tag-rpg-maker-mz/tag-rpgmaker"
 )
 
 ITCHIO_SEEN_FILE = "seen_itchio.json"
