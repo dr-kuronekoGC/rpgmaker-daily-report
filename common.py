98bc4d68fe73a0899f535986464e2c8faa07af80
Dr.Kuroneko <t-ond@hotmail.co.jp>
2026-08-31 00:55:42 +0900

    Update common.py

--- common.py
+++ common.py
@@ -73,3 +73,45 @@ def save_seen_file(
             indent=2,
             ensure_ascii=False,
         )
+
+# ==========================================
+# pending items
+# ==========================================
+
+def load_pending_items(filename: str) -> list:
+    """
+    掲載待ちItemを読み込む
+    """
+
+    path = Path(filename)
+
+    if not path.exists():
+        return []
+
+    with path.open(
+        "r",
+        encoding="utf-8",
+    ) as f:
+        return json.load(f)
+
+
+def save_pending_items(
+    filename: str,
+    data: list,
+) -> None:
+    """
+    掲載待ちItemを保存する
+    """
+
+    path = Path(filename)
+
+    with path.open(
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(
+            data,
+            f,
+            indent=2,
+            ensure_ascii=False,
+        )
